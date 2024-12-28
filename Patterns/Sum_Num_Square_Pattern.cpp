7c8158421732ac19481015aae4c07d1ca2d6e9cb
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2024-12-28 18:53:25 +0530

    Update Sum_Num_Square_Pattern.cpp

--- Patterns/Sum_Num_Square_Pattern.cpp
+++ Patterns/Sum_Num_Square_Pattern.cpp
@@ -4,12 +4,19 @@
 // 9 10 11 12
 // 13 14 15 16
 
+
+// A B C D
+// E F G H 
+// I J K L
+// M N O P
+
 #include<iostream>
 using namespace std;
 int main(){
     int n;
     cout << "Enter Your Number: ";
     cin >> n;
+    // Problem 1
     int sum = 1;
     for(int i = 0;i < n; i++){
         for(int j = 0; j < n;j++){
@@ -18,5 +25,16 @@ int main(){
         }
         cout << endl;
     }
+
+    // Problem 2
+    
+    // char ch = 'A';
+    // for(int i = 0;i < n; i++){
+    //     for(int j = 0; j < n;j++){
+    //         cout << ch << " ";
+    //         ch++;
+    //     }
+    //     cout << endl;
+    // }
     return 0;
 }
