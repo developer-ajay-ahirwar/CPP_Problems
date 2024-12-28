3c8322f173ab735be3c65a58537f07594dd79cad
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2024-12-28 19:53:58 +0530

    Add files via upload

---
+++ Patterns/Floyds_Triangle_Pattern.cpp
@@ -0,0 +1,23 @@
+// Problem
+// 1
+// 2 3
+// 4 5 6 
+// 7 8 9 10
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter Your Number: ";
+    cin >> n;
+    int num = 1;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <=i; j++){
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
