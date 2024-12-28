700831827d8a64680b8b9319ed114f9c3456ed07
Ajay Ahirwar <developer.ajayahirwar@gmail.com>
2024-12-28 19:17:16 +0530

    Add files via upload

---
+++ Patterns/Sum_Num_Right_Half_Pyramid.cpp
@@ -0,0 +1,24 @@
+// Problem
+//1
+//1 2
+//1 2 3
+//1 2 3 4
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter your number: ";
+    cin >> n;
+    for(int i = 0; i < n;i++){
+        int num = 1;
+        for(int j = 0; j <= i; j++){
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+    return 0;
+
+}
